ab856ad85353fecbc9fbc40888f3b357b4d92374
avi <gajai.avinash@gmail.com>
2024-07-11 14:03:48 -0400

    Program plays one game of Rock Paper Scissors

--- code.js
+++ code.js
@@ -1,8 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomInt = Math.random();
     console.log(randomInt);
     let choice = String;
-    if (randomInt >=0 && randomInt < .33) {
+    if (randomInt >= 0 && randomInt < .33) {
         choice = "rock";
     }
     else if (randomInt >= .33 && randomInt < .67) {
@@ -16,7 +19,6 @@ function getComputerChoice() {
 
 }
 
-let computerChoice = getComputerChoice();
 
 function getHumanChoice() {
     userChoice = prompt();
@@ -24,8 +26,56 @@ function getHumanChoice() {
 
 }
 
-let humanChoice = getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
 
-console.log(computerChoice);
-console.log(humanChoice);
+    let lowercaseChoice = humanChoice.toLowerCase();
+    if (lowercaseChoice == "rock" && computerChoice == "paper") {
+        console.log("you lose");
+        computerScore++;
+    }
+    else if (lowercaseChoice == "scissors" && computerChoice == "paper") {
+        console.log("you win");
+        humanScore++;
+    }
+    else if (lowercaseChoice == "paper" && computerChoice == "paper") {
+        console.log("tie");
+    }
 
+
+    else if (lowercaseChoice == "rock" && computerChoice == "rock") {
+        console.log("tie");
+
+    }
+
+    else if (lowercaseChoice == "scissors" && computerChoice == "rock") {
+        console.log("you lose");
+        computerScore++;
+    }
+
+    else if (lowercaseChoice == "paper" && computerChoice == "rock") {
+        console.log("you win");
+        humanScore++;
+    }
+
+    else if (lowercaseChoice == "scissors" && computerChoice == "scissors") {
+        console.log("tie");
+    }
+
+    else if (lowercaseChoice == "rock" && computerChoice == "scissors") {
+        console.log("you win");
+        humanScore++;
+    }
+
+    else if (lowercaseChoice == "paper" && computerChoice == "scissors") {
+        console.log("you lose");
+        computerScore++;
+    }
+
+    return { computerScore, humanScore };
+
+}
+
+playRound();
